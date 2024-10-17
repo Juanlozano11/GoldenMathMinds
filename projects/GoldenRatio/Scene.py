645908be42b88ba4c4a5d451c35457c12c1fbfb1
Juanlozano11 <Juanes.lozano2003@gmail.com>
2024-10-17 11:05:46 -0700

    minor changes to scene

--- projects/GoldenRatio/Scene.py
+++ projects/GoldenRatio/Scene.py
@@ -1,52 +1,53 @@
-from manim import *
+from manim import * 
 
-class GoldenRatio(Scene):
-    def construct(self):
-        # Create two squares
-        square1 = Square(side_length=2, color=BLUE, fill_opacity=0.5)
-        square2 = Square(side_length=1, color=RED, fill_opacity=0.5)
+class S1(Scene):
+       
+       def construct(self):
+        for x in range(-7, 8):
+            for y in range(-4, 5):
+                self.add(Dot(np.array([x, y, 0]), color=BLACK))
 
-        # Position squares
-        square1.shift(LEFT * 3)
-        square2.next_to(square1, RIGHT, buff=0)
+        l1 = Line(start=[(2/(1 + np.sqrt(5) )+ 1) / 2, -0.5, 0], end=[(2/(1 + np.sqrt(5) )+ 1) / 2, 0.5, 0])
 
-        # Calculate golden ratio
-        golden_ratio = (1 + np.sqrt(5)) / 2
+        t_a  = Text("a").shift(LEFT * 2.70) 
+        t_a2 = Text("a").shift(LEFT * 0.85 , DOWN * 1.85 ) 
+        t_b  = Text("b").shift(RIGHT * 1.75 , DOWN * 1.85 )  
+        t1   = Text("This is a Golden Ratio Rectangle").shift(DOWN * 3)
+        t2   = Text("And this is a golden ratio line").shift(DOWN * 3)
+        t3   = Text("But, what does this really mean? ")
 
-        # Create rectangles inside squares
-        rect1 = Rectangle(height=2, width=2/golden_ratio, color=BLUE, fill_opacity=0.5)
-        rect2 = Rectangle(height=1, width=1/golden_ratio, color=RED, fill_opacity=0.5)
+        r1 = Polygon(
+            np.array([ ((2 / (1 + np.sqrt (5))) + 1 )/ 2  ,  0.5, 0]),
+            np.array([ ((2 / (1 + np.sqrt (5))) + 1 )/ 2  , -0.5, 0]),
+            np.array([-((2 / (1 + np.sqrt (5))) + 1 )/ 2  , -0.5, 0]), 
+            np.array([-((2 / (1 + np.sqrt (5))) + 1 )/ 2  ,  0.5, 0])
+            )
+        
+        r2 = Polygon(
+           np.array([ ((2 / (1 + np.sqrt (5))) + 1 )/ 2  , -0.5, 0]),
+            np.array([-((2 / (1 + np.sqrt (5))) + 1 )/ 2  , -0.5, 0]))
+        
+        
+        self.play (Create(r1))
+    
+       
+        self.play(Transform( r1, r1.copy().scale(3) ))
+        self.play(Transform( l1, l1.copy().scale(3) ))
 
-        # Position rectangles
-        rect1.next_to(square1, DOWN, buff=0)
-        rect2.next_to(square2, DOWN, buff=0)
+        self.play (Create(t_a))
+        self.play (Create(t_a2))
+        self.play (Create(t_b))
+        self.play (Create(t1))
 
-        # Create labels
-        label1 = MathTex("a").next_to(square1, UP)
-        label2 = MathTex("b").next_to(square2, UP)
-        label3 = MathTex("a").next_to(rect1, RIGHT)
-        label4 = MathTex("b").next_to(rect2, RIGHT)
-        label5 = MathTex("a").next_to(rect1, DOWN)
-        label6 = MathTex("b").next_to(rect2, DOWN)
 
-        # Show squares, rectangles, and labels
-        self.play(Create(square1), Create(square2))
-        self.play(Create(rect1), Create(rect2))
-        self.play(Write(label1), Write(label2), Write(label3), Write(label4), Write(label5), Write(label6))
 
-        # Add arrows and ratios
-        arrow1 = Arrow(label1.get_right(), label3.get_left(), color=YELLOW)
-        arrow2 = Arrow(label2.get_right(), label4.get_left(), color=YELLOW)
-        ratio1 = MathTex(r"\frac{a}{b}").next_to(arrow1, UP, buff=0.1)
-        ratio2 = MathTex(r"\frac{a}{b}").next_to(arrow2, UP, buff=0.1)
+        self.play(Transform( r1, r2.shift(DOWN).scale(3)) )
+        self.play(Transform( t_a, t_a.copy().scale(0) ))
+        self.play(Transform( l1, l1.copy().shift(DOWN * 1.5).scale(0.4) ))
+        self.play (Transform(t1, t2))
+        self.wait(3)
+        self.play (Create(t3))
 
-        self.play(Create(arrow1), Create(arrow2))
-        self.play(Write(ratio1), Write(ratio2))
 
-        # Explain golden ratio
-        explanation = MathTex(r"\text{The golden ratio: } \frac{a+b}{a} = \frac{a}{b}", color=GREEN).next_to(rect1, RIGHT)
-        explanation.shift(DOWN * 1.3)
-        explanation.shift(RIGHT )
-        self.play(Write(explanation))
-
-        self.wait(2)
+        
+        self.wait(1)
